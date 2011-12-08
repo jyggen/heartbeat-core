55116f6274ecb8a75b38f5f08d2bf1dbfb572c9d
Jonas Stendahl <jonas.stendahl@gmail.com>
2011-12-08 09:13:24 +0100

    Added Controller.class.php.

---
+++ classes/Controller.class.php
@@ -0,0 +1,121 @@
+<?php
+class View
+{
+
+protected $_loader;
+protected $_engine;
+protected $_vars = array();
+
+protected static $_instance = false;
+	
+	public static function getInstance()
+	{
+	
+		if (self::$_instance === false) {
+
+			self::$_instance = new self();
+
+		}
+
+		return self::$_instance;
+
+	}
+
+	public function __construct()
+	{
+
+		try {
+
+			$this->_loader = new Twig_Loader_Filesystem(TEMPLATE_DIR);
+			$this->_engine = new Twig_Environment(
+				$this->_loader,
+				array(
+				 'cache' => CACHE_DIR,
+				 'debug' => DEBUG,
+				 'strict_variables' => true,
+				)
+			);
+
+		} catch (Exception $e) {
+
+			Request::serveErrorPage($e->getMessage());
+
+		}
+
+	}
+
+	public function addFilter($name, $function)
+	{
+
+		try {
+
+			$this->_engine->addFilter(
+				$name,
+				$obj = new Twig_Filter_Function($function)
+			);
+
+		} catch (Exception $e) {
+
+			Request::serveErrorPage($e->getMessage());
+
+		}
+
+	}
+
+	public function addGlobal($name, $var)
+	{
+
+		try {
+
+			$this->_engine->addGlobal($name, $var);
+
+		} catch (Exception $e) {
+
+			Request::serveErrorPage($e->getMessage());
+
+		}
+
+	}
+
+	public function define($key, $var)
+	{
+
+		if (array_key_exists($key, $this->_vars) === false) {
+
+			$this->_vars[$key] = $var;
+
+		} else {
+
+			trigger_error('"'.$key.'" is already defined', E_USER_ERROR);
+			exit(1);
+
+		}
+
+	}
+
+	public function render($template=false)
+	{
+
+		if ($template === false) {
+	
+			$name = substr(Request::$controller, 0, -10);
+			$template = strtolower($name.'_'.Request::$method);
+	
+		}
+	
+		try {
+	
+			$template = $this->_engine->loadTemplate($template.'.twig');
+			$output = $template->render($this->_vars);
+	
+			echo $output;
+	
+		} catch (Exception $e) {
+	
+			throw new Exception($e->getMessage());
+	
+		}
+	
+	}
+	
+}
